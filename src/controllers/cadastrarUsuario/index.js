0ab69618f8cb0b9ffcc6abd0e110b3c4ba851408
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2023-04-26 13:37:06 -0300

    FIX/ Correção de um erro no controller de castro

--- src/controllers/cadastrarUsuario/index.js
+++ src/controllers/cadastrarUsuario/index.js
@@ -8,23 +8,27 @@ export const cadastrarUsuario = async (req, res) => {
     
     //Verificação se não há campos vázios
     if (!usuario || !senha || !confirmacao) {
-      return res.status(200).json({ message: "Preencha todos os campos" , status:400});
+      return res.status(200).json({ mensagem: "Preencha todos os campos" , status:400});
     }
 
+    const usuarioExistente = await servicos.pesquisarUsuarioPorNome(usuario)
+    if(usuarioExistente){
+      return res.status(200).json({mensagem:"Usuário já existe", status:400})
+    }
     //Verificação se as senhas são iguais
     if (senha !== confirmacao) {
-      return res.status(200).json({ message: "As senhas não correspondem", status:400 });
+      return res.status(200).json({ mensagem: "As senhas não correspondem", status:400 });
     }
     //Verificação se a senha tem pelo menos 6 digitos
     if (senha.length < 6) {
-      return res.status(200).json({ message: "A senha deve ter no mínimo 6 caracteres", status:400});
+      return res.status(200).json({ mensagem: "A senha deve ter no mínimo 6 caracteres", status:400});
     }
     
     //Cadastrando o usuário no banco de dados
     try {
       const usuarioServico = await servicos.cadastrarUsuario(req.body);
       const usuarioCriado = { id: usuarioServico._id, usuario };
-      return res.status(201).json({ message: "Usuário cadastrado", usuarioCriado });
+      return res.status(201).json({ mensagem: "Usuário cadastrado", usuarioCriado });
 
     } catch (error) {
       return res.status(500).json({mensagem: "Erro ao criar o usuário", err});
